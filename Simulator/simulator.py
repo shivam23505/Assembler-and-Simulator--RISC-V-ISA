8558fab5fc4ae34da64636def1bb286eb6a4741f
shivam23505 <shivam23505@iiitd.ac.in>
2024-04-15 01:28:56 +0530

    Added main program nad fixed bugs

--- Simulator/simulator.py
+++ Simulator/simulator.py
@@ -1,4 +1,5 @@
 import math
+import sys
 #SIMULATOR FOR RISC-ISA V
 opcode = {"Rtype":"0110011","Stype":"0100011","Btype":"1100011","Jtype":"1101111",
         "Utype":["0110111","0010111"],"Itype":["0000011","0010011","0010011","1100111"]}
@@ -48,6 +49,7 @@ def BinaryConverter(imm):
             m="0"*(32-len(s))
             s=m+s
     return s
+
 register={}
 #Defining register from 0 to 31
 for i in range(0,31):
@@ -95,7 +97,7 @@ def Stype(binary_input):
 def utype(binary_input):
     opcode = binary_input[-7:]
     rd = binary_input[-12:-7]
-    imm = binary_input[21:]+12*"0"
+    imm = binary_input[-32:-12]+12*"0"
     if opcode =="0110111":
         new_value = BinaryConverter(pc + binarytonumber(imm))
         register[rd] = "0b"+new_value
@@ -105,6 +107,91 @@ def utype(binary_input):
 
 def jtype(binary_input):
     rd = binary_input[-12:-7]
-    imm = binary_input[0]+binary_input[12:]+binary_input[11]+binary_input[1:11] +"0"
+    imm = binary_input[0]+binary_input[12:20]+binary_input[11]+binary_input[1:11] +"0"
     register[rd] ="0b" + BinaryConverter(pc+4)
     pc = pc + binarytonumber(imm)
+
+
+def btype(binary_input):
+    funct3 = binary_input[-15:-12]
+    rs1 = binary_input [-18:-15]
+    rs2 = binary_input[-21:-18]
+    imm = binary_input[0] + binary_input[-8] + binary_input[1:7] +binary_input[-12:-8]+"0"
+    value1 = binarytonumber(register[rs1][2:])
+    value2 = binarytonumber(register[rs2][2:])
+    match funct3:
+        case "000":
+            pc=(pc+binarytonumber(imm)) if (value1==value2) else (pc+4)
+        case "001":
+            pc=(pc+binarytonumber(imm)) if (value1!=value2) else (pc+4)
+        case "100":
+            pc=(pc+binarytonumber(imm)) if (value1<value2) else (pc+4)
+        case "101":
+            pc=(pc+binarytonumber(imm)) if (value1>=value2) else (pc+4)
+        case "111":
+            value_1 =int(register[rs1][2:],2)
+            value_2 = int(register[rs2][2:],2)
+            magnitude = int(imm[1:],2)
+            if (imm[0]=="1"):
+                magnitude=-magnitude
+            pc = (pc+magnitude) if (value_1>=value_2) else (pc+4)
+        case "110":
+            value_1 =int(register[rs1][2:],2)
+            value_2 = int(register[rs2][2:],2)
+            magnitude = int(imm[1:],2)
+            if (imm[0]=="1"):
+                magnitude=-magnitude
+            pc = (pc+magnitude) if (value_1<value_2) else (pc+4)
+        case default:
+            return 0
+
+def print_array():
+    ans = []
+    ans.append("0b"+BinaryConverter(pc))
+    for i in register.values():
+        ans.append(i)
+    return ans
+
+
+def main_program(input_path,output_path):
+    with open(input_path) as f:
+        data = f.readlines()
+    for i in data:
+        i.strip()
+    halt = "00000000000000000000000001100011"
+    solutions = []
+    curr_instruction = data[pc/4]
+    while (curr_instruction!=halt):
+        curr_opcode = curr_instruction[-7:]
+        if curr_opcode =="0110011":
+            pass
+        elif curr_opcode =="0100011":
+            Stype(curr_instruction)
+        elif curr_opcode =="1100011":
+            btype(curr_instruction)
+        elif curr_opcode == "1101111":
+            jtype(curr_instruction)
+        elif curr_opcode in ["0110111","0010111"]:
+            utype(curr_instruction)
+        else:
+            itype(curr_instruction)
+
+        solutions.append(" ".join(print_array()))
+        curr_instruction = data[pc/4]
+    with open(output_path,"a") as out: # w modes allows us to refresh output file 
+        for i in solutions:
+            out.write(i+"\n")
+        for i in range(len(memory_locations)-1):
+            out.write(memory_locations[i]+":"+memory_values[memory_locations[i]]+"\n")
+        out.write(memory_locations[-1]+":"+memory_values[memory_locations[-1]])
+
+# if __name__ == "__main__":
+#     if len(sys.argv) != 3:
+#         print("Usage: python3 Filename.py input_file_path output_file_path")
+#         sys.exit(1)
+
+#     input_path = sys.argv[1]
+#     output_path = sys.argv[2]
+
+#     main_program(input_path, output_path)
+
