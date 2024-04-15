9681f7181afdea80ebfd7131fb4a9612ca2106b7
shivam23505 <shivam23505@iiitd.ac.in>
2024-04-16 00:57:17 +0530

    Combined Rtype and Main function

--- Simulator/simulator.py
+++ Simulator/simulator.py
@@ -52,37 +52,178 @@ def BinaryConverter(imm):
 
 register={}
 #Defining register from 0 to 31
-for i in range(0,31):
+for i in range(0,32):
     register[Binary_5_convert(i)]='0b'+'0'*32
 register[Binary_5_convert(2)]='0b'+BinaryConverter(256)
 
-def binarytonumber(bin):
-    #bin is binary_string
-    #returns the corresponding decimal
+def IntTo32Bitsigned(n):
+    # if n < -2147483648 or n > 2147483647:
+    #     raise ValueError("Input integer out of range for 32-bit 2's complement representation")    
+    if n >= 0:
+        binary_str = bin(n)[2:].zfill(32)
+    else:
+        binary_str = bin(2**32 + n)[2:]
+
+    return binary_str
+
+def Signedto32BitsInt(binary_str):
+    # if len(binary_str) != 32:
+    #     raise ValueError("Input string must be exactly 32 characters long")
+
+    if binary_str[0] == '1':
+        # If the most significant bit is 1, it's a negative number
+        # Convert it to its negative equivalent
+        negative_equivalent = int(binary_str, 2) - 2**32
+        return negative_equivalent
+    else:
+        # If the most significant bit is 0, it's a non-negative number
+        return int(binary_str, 2)
+    
+    
+def Signedto32BitsIntU(binary_str):
+    decimal_value = 0
+    for i in range(len(binary_str)):
+        if binary_str[i] == '1':
+            decimal_value += 2**(31 - i)
+
+    return decimal_value
+
+def get_binary_representation(number):
+    binary = ""
+    for i in range(31, -1, -1):
+        binary += "1" if number & (1 << i) else "0"
+    return binary
+
+def get_binary_representation5(number):
+    binary = ""
+    for i in range(4, -1, -1):
+        binary += "1" if number & (1 << i) else "0"
+    return binary
+
+def binary_to_int(binary_str):
+    # if len(binary_str) != 5:
+    #     raise ValueError("Input string must be exactly 5 characters long")
+
+    decimal_value = 0
+    for i in range(len(binary_str)):
+        if binary_str[i] == '1':
+            decimal_value += 2**(4 - i)
+
+    return decimal_value
+
+# def binarytonumber(bin):
+#     count=0
+#     for i in range(0,31):
+#         count=count+int(bin[i])*(pow(2,31-i))
+def binarytonumber_two_complement(bin):
     count=0
-    count=count+int(bin[0])*pow(2,len(bin)-1)*-1
+    count=count+int(bin[0])*pow(2,len(bin)-1)*(-1)
     for i in range(1,len(bin)):
         count=count+int(bin[i])*(pow(2,len(bin)-1-i))
     return count
+def Rtype(binary_input):
+    global pc
+    opcode = binary_input[25:len(binary_input)] #is used to extract the opcode
+    func3 = binary_input[-15:-12]
+    
+    func7 = binary_input[0:7]
+    rs2 = binary_input[7:12]
+    rs1 = binary_input[12:17]
+    rd = binary_input[20:25]
+   
+    rs2val = binarytonumber_two_complement(register[rs2][2:len(register[rs2])])
+    rs1val = binarytonumber_two_complement(register[rs1][2:len(register[rs2])])
+    rdval = binarytonumber_two_complement(register[rd][2:len(register[rd])])
+    pc+=4
+
+    
+    if(func3 == "000" and func7 == "0000000"): # add function
+        rdval = rs1val + rs2val
+        register[rd] = "0b"+ BinaryConverter(rdval)
+        return;
+    
+    if func7 == "0100000" and func3 =="000": # subtract
+        rdval = rs1val - rs2val
+        register[rd ] = "0b" + BinaryConverter(rdval)
+        return
+    
+    if(func3 == "001" and func7 == "0000000"): #from l-5 to l sll instruction
+        #  l =len( register[rs2]);
+         tempstr = register[rs2][-5:] 
+         rdval = rs1val << binary_to_int(tempstr)
+         register[rd ] = "0b" + BinaryConverter(rdval)
+         return 
+     
+    if(func3 == "101" and func7 == "0000000"): #from l-5 to l srl instruction        
+        # l =len( register[rs2]);
+        tempstr = register[rs2][-5:] 
+        rdval = rs1val >> binary_to_int(tempstr)        
+        register[rd ] = "0b" + BinaryConverter(rdval)        
+        return 
+    
+    if(func3 == "110" and func7 == "0000000"): # or function        
+        rdval = rs1val|rs2val
+        register[rd ] = "0b" + BinaryConverter(rdval)        
+        return
+    
+    if(func3 == "111" and func7 == "0000000"): # and function
+        rdval = rs1val&rs2val
+        register[rd ] = "0b" + BinaryConverter(rdval)        
+        return
+    
+    if(func3 == "010" and func7 == "0000000"): # slt type        
+        if(rs1val < rs2val):
+            rdval = 1            
+        else:
+            rdval = 0            
+        register[rd ] = "0b" + BinaryConverter(rdval)        
+        return
+    
+    if(func3 == "011" and func7 == "0000000"): # slt type
+        rs2val = Signedto32BitsIntU(register[rs2][2:len(register[rs2])])
+        rs1val = Signedto32BitsIntU(register[rs1][2:len(register[rs2])])        
+        if(rs1val < rs2val):
+            rdval = 1            
+        else:
+            rdval = 0
+        register[rd ] = "0b" + BinaryConverter(rdval)
+        return
+    
+    if(func3 == "111" and func7 == "0000000"): #bitwise xor
+        rdval = rs1val^rs2val
+        register[rd ] = "0b" + BinaryConverter(rdval)
+        return
+
+
+def binarytonumber(bin):
+    #bin is string
+    return int(bin,2)
+
 #print correct value of jalr in main
 def itype(s):
     s=s[::-1]
     global pc
-    imm=int(s[31:19:-1])
+    imm=(s[31:19:-1])
     rs1=s[19:14:-1]
     func=s[14:11:-1]
     rd=s[11:6:-1]
-    op=s[6:-1:-1]
+    op=s[6::-1] 
     if op=="0000011":
-        register[rd]=memory_values[(binarytonumber(register[rs1][2:]))+binarytonumber(imm)]
+    #convert the memory address into hexadecimal then get the value from the memory location
+    #and do the rest of the work
+        a= hex((binarytonumber_two_complement(register[rs1][2:]))+binarytonumber_two_complement(str(imm)))
+        a=a[0:2] + "0"*(8-(len(a)-2)) + a[2:]
+        register[rd]=memory_values[a]
     elif op=="0010011":
-        register[rd]="0b"+BinaryConverter(binarytonumber(register[rs1][2:])+binarytonumber(imm))
+        register[rd]="0b"+BinaryConverter(binarytonumber_two_complement(register[rs1][2:])+binarytonumber_two_complement(str(imm)))
     else:
-        x=((binarytonumber(register["00111"][2:]))+binarytonumber(imm))
-        if(x%2==0):
-            pc=pc+x
-        else:
-            pc=pc+x-1
+        # x=((binarytonumber(register["00111"][2:]))+binarytonumber(imm))
+        # if(x%2==0):
+        #     pc=pc+x
+        # else:
+        #     pc=pc+x-1
+        pc=binarytonumber_two_complement(register[rs1][2:])
+        return
     pc=pc+4
 
 def Stype(binary_input):
@@ -90,44 +231,49 @@ def Stype(binary_input):
     imm = binary_input[:7] + binary_input[-12:-7]
     rs2 = binary_input[7:12]
     rs1 = binary_input[12:17]
-    new_value = binarytonumber(register[rs1][2:]) + binarytonumber(imm)
-    memory_values[BinaryConverter(new_value)] = register[rs2] 
+    new_value = binarytonumber_two_complement(register[rs1][2:]) + binarytonumber_two_complement(imm)
+    a= hex(new_value)
+    a=a[0:2] + "0"*(8-(len(a)-2)) + a[2:]
+    memory_values[a] = register[rs2] 
     pc+=4
 
 def utype(binary_input):
+    global pc
     opcode = binary_input[-7:]
     rd = binary_input[-12:-7]
     imm = binary_input[-32:-12]+12*"0"
     if opcode =="0110111":
-        new_value = BinaryConverter(pc + binarytonumber(imm))
+        new_value = BinaryConverter(pc + binarytonumber_two_complement(imm))
         register[rd] = "0b"+new_value
     else:
         register[rd] = "0b" + imm
     pc+=4
 
 def jtype(binary_input):
+    global pc
     rd = binary_input[-12:-7]
     imm = binary_input[0]+binary_input[12:20]+binary_input[11]+binary_input[1:11] +"0"
     register[rd] ="0b" + BinaryConverter(pc+4)
-    pc = pc + binarytonumber(imm)
+    pc = pc + binarytonumber_two_complement(imm)
 
 
 def btype(binary_input):
+    global pc
     funct3 = binary_input[-15:-12]
-    rs1 = binary_input [-18:-15]
-    rs2 = binary_input[-21:-18]
+    rs1 = binary_input [-20:-15]
+    rs2 = binary_input[-25:-20]
     imm = binary_input[0] + binary_input[-8] + binary_input[1:7] +binary_input[-12:-8]+"0"
-    value1 = binarytonumber(register[rs1][2:])
-    value2 = binarytonumber(register[rs2][2:])
+    value1 = binarytonumber_two_complement(register[rs1][2:])
+    value2 = binarytonumber_two_complement(register[rs2][2:])
     match funct3:
         case "000":
-            pc=(pc+binarytonumber(imm)) if (value1==value2) else (pc+4)
+            pc=(pc+binarytonumber_two_complement(imm)) if (value1==value2) else (pc+4)
         case "001":
-            pc=(pc+binarytonumber(imm)) if (value1!=value2) else (pc+4)
+            pc=(pc+binarytonumber_two_complement(imm)) if (value1!=value2) else (pc+4)
         case "100":
-            pc=(pc+binarytonumber(imm)) if (value1<value2) else (pc+4)
+            pc=(pc+binarytonumber_two_complement(imm)) if (value1<value2) else (pc+4)
         case "101":
-            pc=(pc+binarytonumber(imm)) if (value1>=value2) else (pc+4)
+            pc=(pc+binarytonumber_two_complement(imm)) if (value1>=value2) else (pc+4)
         case "111":
             value_1 =int(register[rs1][2:],2)
             value_2 = int(register[rs2][2:],2)
@@ -145,7 +291,8 @@ def btype(binary_input):
         case default:
             return 0
 
-def print_array():
+def print_reg_values():
+    global pc
     ans = []
     ans.append("0b"+BinaryConverter(pc))
     for i in register.values():
@@ -154,17 +301,21 @@ def print_array():
 
 
 def main_program(input_path,output_path):
+    global pc
     with open(input_path) as f:
         data = f.readlines()
     for i in data:
         i.strip()
-    halt = "00000000000000000000000001100011"
+    halt = "0"*25+"1100011"
     solutions = []
-    curr_instruction = data[pc/4]
+    curr_instruction = data[pc//4]
+    curr_instruction = curr_instruction[:-1]
+    # print(curr_instruction)
     while (curr_instruction!=halt):
-        curr_opcode = curr_instruction[-7:]
+        print(pc)
+        curr_opcode = curr_instruction[25:32]
         if curr_opcode =="0110011":
-            pass
+            Rtype(curr_instruction)
         elif curr_opcode =="0100011":
             Stype(curr_instruction)
         elif curr_opcode =="1100011":
@@ -176,15 +327,22 @@ def main_program(input_path,output_path):
         else:
             itype(curr_instruction)
 
-        solutions.append(" ".join(print_array()))
-        curr_instruction = data[pc/4]
+        solutions.append(" ".join(print_reg_values()))
+        curr_instruction = data[pc//4]
+        curr_instruction = curr_instruction[:-1]
+    #after halt has been called print again the values
+    solutions.append(" ".join(print_reg_values()))
+
     with open(output_path,"a") as out: # w modes allows us to refresh output file 
         for i in solutions:
-            out.write(i+"\n")
+            out.write(i+" \n")
         for i in range(len(memory_locations)-1):
             out.write(memory_locations[i]+":"+memory_values[memory_locations[i]]+"\n")
-        out.write(memory_locations[-1]+":"+memory_values[memory_locations[-1]])
+        out.write(memory_locations[-1]+":"+memory_values[memory_locations[-1]]+" ")
 
+
+
+main_program("input.txt","output.txt")
 # if __name__ == "__main__":
 #     if len(sys.argv) != 3:
 #         print("Usage: python3 Filename.py input_file_path output_file_path")
